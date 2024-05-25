15f1f1e31e6b3caf18fd3ac93bda56be3df7b381
ch-yang1273 <ych1273@gmail.com>
2024-05-25 20:37:31 +0900

    typescript

--- TypeScript/motion_answer/src/app.ts
+++ TypeScript/motion_answer/src/app.ts
@@ -1,26 +1,27 @@
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
+import { Component } from './components/component.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
 
     const video: VideoComponent = new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=LwG1g5kFIWM');
-    video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
 
     const note: NoteComponent = new NoteComponent('Note Title', 'Note Content');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo: TodoComponent = new TodoComponent('Todo Tile', 'Todo Item');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
   }
 }
 
