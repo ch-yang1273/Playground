9e4a4513d2634830b0ebcea7a9eb89ee664606e1
ch-yang1273 <ych1273@gmail.com>
2025-05-26 17:19:14 +0900

    CSS Loader

--- 2025/Webpack_01/src/index.js
+++ 2025/Webpack_01/src/index.js
@@ -1,4 +1,6 @@
 import hello_word from "./hello.js";
 import world_word from "./world.js";
+import css from "./style.css";
 
 document.getElementById('root').innerHTML = hello_word + ' ' + world_word;
+console.log('style css', css);
--- 2025/Webpack_01/webpack.config.js
+++ 2025/Webpack_01/webpack.config.js
@@ -7,5 +7,15 @@ module.exports = {
         filename: 'index_bundle.js',
     },
     mode: 'development',
-    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+        ],
+    },
 };
